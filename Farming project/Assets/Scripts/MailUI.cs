4cd79d98674038d04c114759062a4db72a54468d
ThatIsANoNoWord <dharmawan.jo@gmail.com>
2025-04-06 17:38:32 -0500

    Made Mail come from an actual Mailbox

--- Farming project/Assets/Scripts/MailUI.cs
+++ Farming project/Assets/Scripts/MailUI.cs
@@ -8,14 +8,20 @@ public class MailUI : UI, ITurnable
 {
     public TextMeshProUGUI mailContents;
     public GameObject interactor;
+    public Sprite mailPresent;
+    public Sprite mailAbsent;
     PlayerController playerController;
     Queue<string> mailQueue;
+    Collider2D interactorCollider;
+    SpriteRenderer interactorRenderer;
     private void Start()
     {
         playerController = FindObjectOfType<PlayerController>();
         mailQueue = new Queue<string>();
 
         gameObject.SetActive(false);
+        interactorCollider = interactor.GetComponent<Collider2D>();
+        interactorRenderer = interactor.GetComponent<SpriteRenderer>();
     }
     public void ReturnControl()
     {
@@ -27,18 +33,25 @@ public class MailUI : UI, ITurnable
     {
         mailQueue.Enqueue(content);
         interactor.SetActive(true);
+        interactorCollider.enabled = true;
+        interactorRenderer.sprite = mailPresent;
     }
     public override void UpdateData()
     {
         if (mailQueue.Count == 0) return;
         mailContents.text = mailQueue.Dequeue();
-        if (mailQueue.Count == 0) interactor.SetActive(false);
+        if (mailQueue.Count == 0)
+        {
+            interactorCollider.enabled = false;
+            interactorRenderer.sprite = mailAbsent;
+        }
     }
 
     public void Turn()
     {
         mailQueue.Clear();
-        interactor.SetActive(false);
+        interactorCollider.enabled = false;
+        interactorRenderer.sprite = mailAbsent;
     }
 
     public int Prio()
